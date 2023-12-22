e509ddcebdb99baf586f87d66f7a8742fcf794f8
MaysNajjar <56773602+mays-najjar@users.noreply.github.com>
2023-12-22 14:16:06 +0200

    BalancetheParentheses.dart

---
+++ BalancetheParentheses.dart
@@ -0,0 +1,30 @@
+import 'stack.dart';
+import 'dart:io';
+
+bool isBalanced(String str) {
+  Stack<String> stack = Stack<String>();
+
+for (String char in str.split('')) {
+  if (char == '(') {
+    stack.push(char); // all ( will be in this stack 
+  } else if (char == ')') {  
+    if (stack.isEmpty || stack.pop() != '(') {
+      return false;
+    }
+  }
+}  return stack.isEmpty;
+
+}
+
+void main() {
+  print("Enter a string with parentheses: ");
+  String userInput = stdin.readLineSync() ?? "";
+  
+  bool result = isBalancedParentheses(userInput);
+
+  if (result) {
+    print("Parentheses are balanced.");
+  } else {
+    print("Parentheses are not balanced.");
+  }
+}
